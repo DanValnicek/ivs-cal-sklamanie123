a2f5bf296c021f3ef6b0d05cfd6ab580a4c40ef7
abs3nce <vodrazka.jakub@gmail.com>
2023-03-25 11:11:15 +0100

    > fixed file structure and test config

--- src/lib/src/calc.ts
+++ src/lib/src/calc.ts
@@ -1,10 +1,4 @@
 // testing file to see if jest works
-let x: number = 10;
-
-let y: number = 20;
-
 export function add(x: number, y: number): number {
   return x + y;
 }
-
-console.log(add(x, y));
--- src/lib/src/tests/calc.test.ts
+++ src/lib/src/tests/calc.test.ts
@@ -1,4 +1,4 @@
-import { add } from './calc';
+import { add } from '../calc';
 describe('test add function', () => {
   it('should return 15 for add(10,5)', () => {
     expect(add(10, 5)).toBe(15);
