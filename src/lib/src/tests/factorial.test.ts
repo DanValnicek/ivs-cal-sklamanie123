cb21f23ee2c24f5fc7806e441c91111ad7ec2731
abs3nce <vodrazka.jakub@gmail.com>
2023-03-25 18:20:14 +0100

    /tests: changed expected outputs of non valid operations

--- src/lib/src/tests/factorial.test.ts
+++ src/lib/src/tests/factorial.test.ts
@@ -6,13 +6,16 @@ describe('test factorial function', () => {
   it('should return 1 for factorial(0)', () => {
     expect(factorialOfNumber(0)).toBe(1);
   });
-  it('should return error for factorial(-5)', () => {
-    expect(factorialOfNumber(-5)).toThrow('Input number must be postive');
+  it('should return 1 for factorial(1)', () => {
+    expect(factorialOfNumber(1)).toBe(1);
   });
-  it('should return error for factorial(1.5)', () => {
-    expect(factorialOfNumber(1.5)).toThrow('Input number must be whole');
+  it('should return undefined for factorial(1.5)', () => {
+    expect(factorialOfNumber(1.5)).toBe(undefined);
   });
-  it('should return error for factorial(-1.5)', () => {
-    expect(factorialOfNumber(-1.5)).toThrow('Input number must be whole and positive');
+  it('should return undefined for factorial(-5)', () => {
+    expect(factorialOfNumber(-5)).toBe(undefined);
+  });
+  it('should return undefined for factorial(-1.5)', () => {
+    expect(factorialOfNumber(-1.5)).toBe(undefined);
   });
 });
