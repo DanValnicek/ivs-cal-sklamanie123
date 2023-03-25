786c6952ba5196be2e747aa5ffd7b17bf1f7ca8b
abs3nce <vodrazka.jakub@gmail.com>
2023-03-25 10:52:22 +0100

    +tests: addition

---
+++ src/lib/calc.ts
@@ -0,0 +1,10 @@
+// testing file to see if jest works
+let x: number = 10;
+
+let y: number = 20;
+
+export function add(x: number, y: number): number {
+  return x + y;
+}
+
+console.log(add(x, y));
