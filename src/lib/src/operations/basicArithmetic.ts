f25b997ab1b943a61d8a87426e6673dd33c17827
Lukáš Katona <katona.lukasek@gmail.com>
2023-03-29 13:41:52 +0200

    + basic aritmetinc functions

--- src/lib/src/operations/basicArithmetic.ts
+++ src/lib/src/operations/basicArithmetic.ts
@@ -1,13 +1,20 @@
 
     export function add(x: number, y: number): number{
-
+        const result = (x + y).toFixed(9);
+        return parseFloat(result);
     }
     export function substract(x: number, y: number): number{
-
+        const result = (x - y).toFixed(9);
+        return parseFloat(result);
     }
     export function multiply(x: number, y: number): number{
-
+        const result = (x * y).toFixed(9);
+        return parseFloat(result);
     }
     export function divide(x: number, y: number): number | undefined{
-
+        if(y === 0){
+            return undefined;
+        }
+        const result = (x / y).toFixed(9);
+        return parseFloat(result);
     }
