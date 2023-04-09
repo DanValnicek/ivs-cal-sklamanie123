d9b2a0c47f4949fa9bc1986baa4191eefccdf15c
Lukáš Katona <katona.lukasek@gmail.com>
2023-04-09 09:58:10 +0200

    + absolute value function, interface and tests

--- src/lib/src/operations/basicArithmetic.ts
+++ src/lib/src/operations/basicArithmetic.ts
@@ -18,3 +18,11 @@
         const result = (x / y).toFixed(9);
         return parseFloat(result);
     }
+
+    export function absolute(x: number): number{
+        let result = (x).toFixed(9);
+        if (x < 0){
+            result = (x * -1).toFixed(9);
+        }
+        return parseFloat(result);
+    }
