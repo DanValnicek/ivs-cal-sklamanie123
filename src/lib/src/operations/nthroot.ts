ecabbecf2aa6e93fd841d4e29ec0407e847a4055
DanValnicek <danvalnicek@gmail.com>
2023-04-10 22:02:25 +0200

    / nThRoot infinite loop and nThRoot tests argument order

--- src/lib/src/operations/nthroot.ts
+++ src/lib/src/operations/nthroot.ts
@@ -1,21 +1,5 @@
 export function nThRoot(n: number, x: number): number | undefined {
-    /*
-     y = n root x
-     y^n = x
-     y^n = 0
-     Newton-Rhapson approximation method
-        a_n+1 = a_0 - (f(a_n)/f'(a_n)
-     */
-    // return Math.pow(x,1/n)
-    const tolerance = 1e-9;
-    //initial guess 1+(x−1)÷n
-    let a, a_1 = 1 + (x - 1) / n;
-    // let a, a_1 = x;
-    do {
-        a = a_1;
-        a_1 = a - (a ** n - x) / (n * a ** (n - 1));
-    } while (Math.abs(a - a_1) > tolerance);
-    if(isNaN(a_1))return  undefined;
-    a_1 = Math.round(a_1 * 1e9) / 1e9
-    return a_1;
+    let result = Math.pow(x, 1 / n);
+    if (isNaN(result) || result === Infinity) return undefined;
+    return Math.round(result * 1e9) / 1e9;
 }
--- src/lib/src/tests/nthroot.test.ts
+++ src/lib/src/tests/nthroot.test.ts
@@ -1,52 +1,52 @@
 import { nThRoot } from '../operations/nthroot';
 describe('test nth root function', () => {
   it('should return 3', () => {
-    expect(nThRoot(9, 2)).toBe(3);
+    expect(nThRoot(2,9 )).toBe(3);
   });
   it('should return 2', () => {
-    expect(nThRoot(8, 3)).toBe(2);
+    expect(nThRoot(3,8 )).toBe(2);
   });
   it('should return given number', () => {
-    expect(nThRoot(9, 1)).toBe(9);
+    expect(nThRoot(1,9 )).toBe(9);
   });
 
   it('should return given numbers mirror value', () => {
-    expect(nThRoot(4, -1)).toBe(0.25);
+    expect(nThRoot(-1,4 )).toBe(0.25);
   });
   it('should return given numbers mirror value', () => {
-    expect(nThRoot(1000, -1)).toBe(0.001);
+    expect(nThRoot(-1,1000 )).toBe(0.001);
   });
 
   it('root -0.1 of 0', () => {
-    expect(nThRoot(0, -0.1)).toBe(undefined);
+    expect(nThRoot(-0.1,0 )).toBe(undefined);
   });
   it('root 0.1 of 0', () => {
-    expect(nThRoot(0, 0.1)).toBe(0);
+    expect(nThRoot(0.1,0 )).toBe(0);
   });
   it('root -1 of 0', () => {
-    expect(nThRoot(0, -1)).toBe(undefined);
+    expect(nThRoot(-1,0 )).toBe(undefined);
   });
   it('root 1 of 0', () => {
-    expect(nThRoot(0, 1)).toBe(0);
+    expect(nThRoot(1,0 )).toBe(0);
   });
 
   it('root -3 of 8', () => {
-    expect(nThRoot(8, -3)).toBe(0.5);
+    expect(nThRoot(-3,8 )).toBe(0.5);
   });
   it('root -1.5 of 8', () => {
-    expect(nThRoot(8, -1.5)).toBe(0.25);
+    expect(nThRoot(-1.5,8 )).toBe(0.25);
   });
 
   it('root 0.3 of 8', () => {
-    expect(nThRoot(8, 0.3)).toBe(1024);
+    expect(nThRoot(0.3,8 )).toBe(1024);
   });
   it('root 0.5 of 8', () => {
-    expect(nThRoot(8, 0.5)).toBe(64);
+    expect(nThRoot(0.5,8 )).toBe(64);
   });
   it('root -0.6 of 8', () => {
-    expect(nThRoot(8, -0.6)).toBe(0.03125);
+    expect(nThRoot(-0.6,8 )).toBe(0.03125);
   });
   it('root -0.5 of 4', () => {
-    expect(nThRoot(4, -0.5)).toBe(0.0625);
+    expect(nThRoot(-0.5,4 )).toBe(0.0625);
   });
 });
