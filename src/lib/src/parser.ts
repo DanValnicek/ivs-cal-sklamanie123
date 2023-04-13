0ce4e55b91364f80e58d34b08c2f049fd4449bbf
abs3nce <vodrazka.jakub@gmail.com>
2023-04-14 00:03:33 +0200

    + validation checks sanitizeInput (lib) & / edge cases

--- src/lib/src/parser.ts
+++ src/lib/src/parser.ts
@@ -129,41 +129,70 @@ export function parseExpression(expression: string): number | undefined {
 }
 
 export function sanitizeInput(input: string): string {
-    // replace all occurrences of "|x|" with "abs ( x )"
-    let regex = /\|(.?\d+(\.\d+)?|\w+)\|/g;
-    input = input.replace(regex, 'abs ( $1 )');
+    let regex;
 
-    // replace all occurrences of "root(x,n)" with "root n ( x )"
-    regex = /root\((.?\d+(\.\d+)?|\w+),(\d+)\)/g;
-    input = input.replace(regex, 'root $3 ( $1 )');
+    // return input (invalid) if it contains two numbers separated by any number of spaces
+    regex = /\d\s\d/g;
+    if (input.match(regex)) {
+        return input;
+    }
+
+    // remove all spaces
+    input = input.replace(/\s/g, '');
+
+    // =================================================================
+
+    // replace all occurrences of "|x|" with "abs ( x )"
+    regex = /\|(.?\d+(\.\d+)?|\w+)\|/g;
+    input = input.replace(regex, "abs ( $1 )");
+
+    // replace all occurrences of "abs(x)" with "abs ( x )"
+    regex = /abs\((.+?)\)/g;
+    input = input.replace(regex, "abs ( $1 )");
+
+    // =================================================================
+
+    // replace all occurrences of "rootY(X)" with "root Y ( X )"
+    regex = /root(\d+)\((.+?)\)/g;
+    input = input.replace(regex, "root $1 ( $2 )");
+
+    // =================================================================
 
     // replace all occurrences of "x!" with "fact ( x )"
     regex = /(-?\d+(\.\d+)?|\w+)!/g;
-    input = input.replace(regex, 'fact ( $1 )');
+    input = input.replace(regex, "fact ( $1 )");
 
-    // add space before and after every + * /
-    regex = /(\+|\*|\/)/g;
-    input = input.replace(regex, ' $1 ');
+    // replace all occurrences of "fact(x)" with "fact ( x )"
+    regex = /fact\((.+?)\)/g;
+    input = input.replace(regex, "fact ( $1 )");
+
+    // =================================================================
+
+    // add space before and after every + * / ^
+    regex = /(\+|\*|\/|\^)/g;
+    input = input.replace(regex, " $1 ");
 
     // add space after every - that if followed by function abs, root of fact
     regex = /-(abs|root|fact)/g;
-    input = input.replace(regex, ' - $1');
+    input = input.replace(regex, " - $1");
 
-    // add space before and after "-" when there is a ')' before it
+    // add space before and after "-" when there is a ')' before it 
     regex = /\)(-)/g;
-    input = input.replace(regex, ') $1 ');
+    input = input.replace(regex, ") $1 ");
 
     // add space only before "-" when there is "(" before it
     regex = /(\()(-)/g;
-    input = input.replace(regex, '$1 $2');
+    input = input.replace(regex, "$1 $2");
 
     // add space before and after "-" when there is a number before it
     regex = /(\d)(-)/g;
-    input = input.replace(regex, '$1 $2 ');
+    input = input.replace(regex, "$1 $2 ");
 
-    // remove any double spaces
+    // =================================================================
+
+    // remove any access spaces
     regex = /\s\s/g;
-    input = input.replace(regex, ' ');
+    input = input.replace(regex, " ");
 
     return input;
 }
