7dfd9a5a326d60da111a68e142c2a5a810e58889
Lukáš Katona <katona.lukasek@gmail.com>
2023-04-15 15:14:55 +0200

    * sum function and tests

--- src/lib/src/operations/sum.ts
+++ src/lib/src/operations/sum.ts
@@ -1,11 +1,17 @@
     import { parseExpression } from '../parser';
     
-    export function sum(m: number, n: number, term: string): number{
+    export function sum(m: number, n: number, term: string): number | undefined{
+        let tmpResult;
         let result = 0;
-        let tmpTerm = '';
+        let tmpTerm = '0';
         for(let i = m; i <= n; i++){
             tmpTerm = term.replace('n', i.toString());
-            result += parseExpression(tmpTerm);
+            tmpResult = parseExpression(tmpTerm);
+            if(tmpResult == undefined){
+                return undefined;
+            } else {
+                result += tmpResult;
+            }
         }
         return result;
     }
