fda2d09acd0f7b093b5c410c597cc65972d8f10e
Lukáš Katona <katona.lukasek@gmail.com>
2023-04-15 15:58:20 +0200

    + sum tests for undefined result

--- src/lib/src/tests/sum.test.ts
+++ src/lib/src/tests/sum.test.ts
@@ -8,5 +8,7 @@ describe('test sum function', () => {
     it('should return -10', () => expect(sum(0, 4, 'n - 4')).toBe(-10));
     it('should return 152', () => expect(sum(2, 5, 'fact ( n )')).toBe(152));
     it('should return 4', () => expect(sum(2, 5, 'abs ( n - 3 )')).toBe(4));
-    it('should return 0', () => expect(sum(2, 5, 'root 2 ( n )')).toBe(7.382332347));
+    it('should return 7.382332347', () => expect(sum(2, 5, 'root 2 ( n )')).toBe(7.382332347));
+    it('should return undefined', () => expect(sum(2, 5, 'n / 0')).toBeUndefined());
+    it('should return undefined', () => expect(sum(-2, 5, 'root 2 ( n )')).toBeUndefined());
 });
