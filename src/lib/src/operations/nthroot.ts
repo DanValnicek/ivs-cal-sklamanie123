c101745feb152da2431c7ec189ba9f38e3bf929b
Lukáš Katona <katona.lukasek@gmail.com>
2023-04-24 16:37:14 +0200

    + documentation of lin ts files

--- src/lib/src/operations/nthroot.ts
+++ src/lib/src/operations/nthroot.ts
@@ -1,3 +1,15 @@
+/**
+ * @file nthroot.ts
+ * @brief nth root operation
+ * @author Dan Valníček
+ */
+
+/**
+ * calculates nth root of a number
+ * @param n index of root
+ * @param x radicand
+ * @returns nth root of x
+ */
 export function nThRoot(n: number, x: number): number | undefined {
     let result = Math.pow(x, 1 / n);
     if (isNaN(result) || result === Infinity) return undefined;
