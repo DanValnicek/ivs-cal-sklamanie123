ff6176c087d9dfd90af72ff38dd673e8617f93af
DanValnicek <danvalnicek@gmail.com>
2023-04-24 16:09:15 +0200

    > updated let to const, moved require and changed exception message

--- src/profiling/standardDeviation.ts
+++ src/profiling/standardDeviation.ts
@@ -1,18 +1,18 @@
 import {parseExpression} from '../lib/src/parser';
+const fs = require("fs");
 
 async function standardDeviation() {
-    const fs = require("fs");
     const input = fs.readFileSync(0, "utf-8");
 
-    let sumOfValues = input.replace(/\s+/g, " + ");
-    let values = input.match(/[-]?\d+(\.\d+)?/g);
-    let N = values.length;
-    let mean = parseExpression(`( ${sumOfValues} ) / ${N}`, false);
+    const sumOfValues = input.replace(/\s+/g, " + ");
+    const values = input.match(/[-]?\d+(\.\d+)?/g);
+    const N = values.length;
+    const mean = parseExpression(`( ${sumOfValues} ) / ${N}`, false);
     let sum = 0;
     for (let i = 0; i < N; i++) {
         sum += parseExpression(`( ${values[i]} - ${mean} ) ^ 2`, false);
     }
-    let standardDeviation = parseExpression(`root 2 ( ${sum} / ( ${N} - 1 ) )`, false);
+    const standardDeviation = parseExpression(`root 2 ( ${sum} / ( ${N} - 1 ) )`, false);
 
     console.log(standardDeviation);
 }
