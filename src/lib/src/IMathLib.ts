051c15218d4b10f750de06f7c3dcce69c8b78106
Lukáš Katona <katona.lukasek@gmail.com>
2023-04-24 16:50:57 +0200

    - documentation from lib interface

--- src/lib/src/IMathLib.ts
+++ src/lib/src/IMathLib.ts
@@ -15,13 +15,6 @@ interface IMathLib {
 
     nThRoot(n: number, x: number): number | undefined;
 
-    /**
-     * Sum function denoted by capital sigma notation
-     *https://en.wikipedia.org/wiki/Summation#:~:text=Capital%2Dsigma%20notation%5B,n.%5Bb%5D
-     * @param m lower bound of summation
-     * @param n upper bound of summation
-     * @param term to be summed
-     */
     sum(m: number, n: number, term: string): number | undefined;
 
     parseExpression(expression: string): number | undefined;
